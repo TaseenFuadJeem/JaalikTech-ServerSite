2f170c0c99951b29fb0c02cb122e8315ae1d30b0
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-12-11 19:40:19 +0600

    Added pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,30 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#27a9e0",
+
+          "secondary": "#2a3a91",
+
+          "accent": "#1FB2A6",
+
+          "neutral": "#191D24",
+
+          "base-100": "#ffffff",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
 }
