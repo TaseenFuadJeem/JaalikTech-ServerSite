5fa5c8390d4c3030bcf3a12318a881ff1121c54f
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-12-19 18:34:14 +0600

    Minor updates

--- src/Resources/Pages/Dashboard/Bookings.js
+++ src/Resources/Pages/Dashboard/Bookings.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../All-Components/Common/Loading';
 
 const Bookings = () => {
+
+    // Fetching data from database for showing all bookings
+    const { data: bookings, isLoading } = useQuery("AllBookingList", () => fetch("http://localhost:5000/all-bookings")
+        .then(res => res.json())
+    );
+
+    // Loading for database
+    if (isLoading) {
+        return <Loading />
+    }
+
     return (
         <div className='bg-white mx-4 mb-4 p-4 rounded-md'>
-            <h1 className='text-xl font-semibold'>Manage All Bookings</h1>
+            <h1 className='text-2xl font-semibold'>Manage All Appointments</h1>
+
+            <div className='mt-5'>
+                <div className="overflow-x-auto border rounded-lg">
+                    <table className="table table-compact w-full">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Country</th>
+                                <th>Phone Num</th>
+                                <th>Appointment Date</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        {
+                            bookings?.map((booking, index) =>
+
+                                <tbody>
+                                    <tr>
+                                        <th>{index + 1}</th>
+                                        <td>{booking?.name}</td>
+                                        <td>{booking?.email}</td>
+                                        <td>{booking?.country}</td>
+                                        <td>{booking?.phone}</td>
+                                        <td>{booking?.date}</td>
+                                        <td><button className="btn btn-xs btn-warning capitalize text-white">{booking?.status}</button></td>
+                                    </tr>
+                                </tbody>
+
+                            )
+                        }
+                        {/*  */}
+                    </table>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/Resources/Pages/Dashboard/Dashboard.js
+++ src/Resources/Pages/Dashboard/Dashboard.js
@@ -42,7 +42,7 @@ const Dashboard = () => {
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                <ul className="menu p-4 lg:w-[250px] w-80 bg-base-100 text-base-content">
                     {/* Sidebar content here */}
                     <li className='my-2'><NavLink to="/dashboard/">Appointment Bookings</NavLink></li>
                     <li className='my-2'><NavLink to="/dashboard/manage-articles-and-blogs">Articles and Blogs</NavLink></li>
